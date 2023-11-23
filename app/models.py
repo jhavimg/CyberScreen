00161d416c5063e5ce0705a8a0e925bb16d76d04
Federico-gvgv <federicogvgv@gmail.com>
2023-11-23 18:51:42 +0100

    Tablas RRHH

--- app/models.py
+++ app/models.py
@@ -6,7 +6,7 @@ from django.apps import apps
 
 class Ingreso(models.Model):
     Codigo = models.CharField(max_length=8, primary_key=True)
-    Fecha = models.DateField()
+    Fecha = models.DateField() 
     Autor = models.CharField(max_length=50)
     Cantidad = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0)])
 
@@ -14,3 +14,32 @@ class Ingreso(models.Model):
         return self.Codigo
     
 Ingreso._meta.app_label = 'app'
+
+class Trabajador(models.Model):
+    DNIT = models.CharField(max_length=9, primary_key=True)
+    Nombre = models.CharField(max_length=20)
+    Apellidos = models.CharField(max_length=40)
+    Teléfono = models.CharField(max_length=9)
+    CorreoElectronico = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.Nombre} {self.Apellidos}"
+
+class Departamento(models.Model):
+    NombreDep = models.CharField(max_length=20, primary_key=True)
+    Descripcion = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.NombreDep
+
+class Pertenece(models.Model):
+    DNIT = models.CharField(max_length=9, primary_key=True)
+    NombreDep = models.CharField(max_length=20)
+    trabajador = models.ForeignKey('Trabajador', on_delete=models.CASCADE)
+    departamento = models.ForeignKey('Departamento', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.DNIT} - {self.NombreDep}"
+
+
+
