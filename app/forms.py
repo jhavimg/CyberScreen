85b6a4cb4ef9070e8049a58ae54acd9358fb3b70
adrianrv007 <adrianrovil007@gmail.com>
2023-11-29 17:32:51 +0100

    Cliente-Suscripcion-Obtiene---Models-Form

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,23 @@
 # mi_aplicacion/forms.py
 from django import forms
-from .models import Ingreso
+from .models import *
 
 class IngresoForm(forms.ModelForm):
     class Meta:
         model = Ingreso
         fields = ['Codigo', 'Fecha', 'Autor', 'Cantidad']
+
+class ClienteForm(forms.ModelForm):
+    class Meta:
+        model = Cliente
+        fields = ['DNICl', 'Telefono', 'Nombre', 'Correo']
+
+class SuscripcionForm(forms.ModelForm):
+    class Meta:
+        model = Suscripcion
+        fields = ['DatosSuscripcion', 'Tipo', 'Nivel']
+
+class ObtieneForm(forms.ModelForm):
+    class Meta:
+        model = Obtiene
+        fields = ['DNICl', 'DatosSuscripcion']
--- app/models.py
+++ app/models.py
@@ -14,3 +14,38 @@ class Ingreso(models.Model):
         return self.Codigo
     
 Ingreso._meta.app_label = 'app'
+
+class Cliente(models.Model):
+    DNICl = models.CharField(max_length=9, primary_key=True)
+    Teléfono = models.PositiveIntegerField()
+    Nombre = models.CharField(max_length=40)
+    Correo = models.EmailField(max_length=30)
+
+    def __str__(self):
+        return self.Nombre
+    
+Cliente._meta.app_label = 'app'
+
+class Suscripcion(models.Model):
+    DatosSuscripcion = models.CharField(max_length=30, primary_key=True)
+    Tipo = models.CharField(max_length=10, choices=[('Mensual', 'Mensual'), ('Anual', 'Anual')], default='Mensual')
+    Nivel = models.CharField(max_length=10, choices=[('Estandar', 'Estandar'), ('Premium', 'Premium')], default='Estandar')
+
+    def __str__(self):
+        return self.DatosSuscripcion
+    
+Suscripcion._meta.app_label = 'app'
+
+class Obtiene(models.Model):
+    DatosSuscripcion = models.CharField(max_length=30, primary_key=True)
+    DNICl = models.CharField(max_length=9, unique=True)
+    Precio = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0)])
+    
+    # Definición de las claves foráneas
+    suscripcion = models.ForeignKey('Suscripcion', on_delete=models.CASCADE, related_name='obtenciones')
+    cliente = models.ForeignKey('Cliente', on_delete=models.CASCADE, related_name='obtenciones')
+
+    def __str__(self):
+        return f"{self.DatosSuscripcion} - {self.DNICl}"
+    
+Obtiene._meta.app_label = 'app'
